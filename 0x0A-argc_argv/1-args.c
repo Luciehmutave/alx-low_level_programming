f1236e8401192b253e02ea9c7e949e5566eadfed
Luciehmutave <luciehmutave020@gmail.com>
2023-09-05 00:08:25 -0700

    Silence is argument

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - print the number of arguments passed
@@ -12,13 +12,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	(void) argv; /*ignore argv*/
 
-	for (i = 0; i < argc; i++)
+		printf("%d\n", argc - 1);
 
-	{
-		printf("%s\n", argv[i]);
-	}
-
-	return (0);
+		return (0);
 }
