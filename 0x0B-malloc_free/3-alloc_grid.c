a4dd8dced8c681dae4d95544e05ebcad259ffd24
Luciehmutave <luciehmutave020@gmail.com>
2023-09-06 11:35:49 -0700

    It's not bragging

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 
 		return (NULL);
-	mee = malloc(sizeof(int*) * height)
+	mee = malloc(sizeof(int*) * height);
 
 	if (mee == NULL)
 		return (NULL);
@@ -36,7 +36,7 @@ int **alloc_grid(int width, int height)
 
 	for (x = 0; x < height; x++)
 	{
-		for (y = o; y < width; y++)
+		for (y = 0; y < width; y++)
 			mee[x][y] = 0;
 	}
 	return (mee);
